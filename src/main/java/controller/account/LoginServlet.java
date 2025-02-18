b3971a4573f8d04c44ffd29a76a3c6372b5d4908
tienminhthanh <thanhtmce181941@fpt.edu.vn>
2025-02-18 17:41:48 +0700

    Da sua cai Login voi xac thuc Account

--- src/main/java/controller/account/LoginServlet.java
+++ src/main/java/controller/account/LoginServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
@@ -27,43 +28,47 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        Account account = accountDAO.getAccountByUsername(username);
+        try {
+            Account account = accountDAO.getAccountByUsername(username);
 
-        if (account != null) {
-            if (account.getIsActive()) {
-                if (account.getPassword().equals(password)) {
-                    HttpSession session = request.getSession();
-                    session.setAttribute("account", account);
-                    switch (account.getRole()) {
-                        case "admin":
-                            response.sendRedirect("dashboard.jsp");
-                            break;
-                        case "customer":
-                            response.sendRedirect("home.jsp");
-                            break;
-                        case "staff":
-                            response.sendRedirect("dashboard.jsp");
-                            break;
-                        case "shipper":
-                            response.sendRedirect("shipperDashboard.jsp");
-                            break;
-                        default:
-                            session.invalidate();
-                            request.setAttribute("errorMessage", "Invalid access!");
-                            request.getRequestDispatcher("login.jsp").forward(request, response);
-                            break;
+            if (account != null) {
+                if (account.getIsActive()) {
+                    if (account.getPassword().equals(password)) {
+                        HttpSession session = request.getSession();
+                        session.setAttribute("account", account);
+                        switch (account.getRole()) {
+                            case "admin":
+                                response.sendRedirect("dashboard.jsp");
+                                break;
+                            case "customer":
+                                response.sendRedirect("home.jsp");
+                                break;
+                            case "staff":
+                                response.sendRedirect("dashboard.jsp");
+                                break;
+                            case "shipper":
+                                response.sendRedirect("shipperDashboard.jsp");
+                                break;
+                            default:
+                                session.invalidate();
+                                request.setAttribute("errorMessage", "Invalid access!");
+                                request.getRequestDispatcher("login.jsp").forward(request, response);
+                                break;
+                        }
+                    } else {
+                        request.setAttribute("errorMessage", "Wrong password!");
+                        request.getRequestDispatcher("login.jsp").forward(request, response);
                     }
                 } else {
-                    request.setAttribute("errorMessage", "Wrong password!");
+                    request.setAttribute("errorMessage", "Your account is deactivated or locked!");
                     request.getRequestDispatcher("login.jsp").forward(request, response);
                 }
             } else {
-                request.setAttribute("errorMessage", "Your account is deactivated or locked!");
+                request.setAttribute("errorMessage", "Account not found!");
                 request.getRequestDispatcher("login.jsp").forward(request, response);
             }
-        } else {
-            request.setAttribute("errorMessage", "Account not found!");
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
