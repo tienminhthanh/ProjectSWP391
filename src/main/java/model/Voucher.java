b4d3b8297d5a4e88e9b8e405f17ce9c6ddb39aae
ADMIN <ADMIN@DESKTOP-9IDIGFR>
2025-02-21 23:07:53 +0700

    add Voucher model

---
+++ src/main/java/model/Voucher.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author trungtinh
+ */
+public class Voucher {
+
+    private int voucherID;
+    private String voucherName;
+    private int voucherValue;
+    private int quantity;
+    private int minimumPurchaseAmount;
+    private String dateCreated;
+    private int duration;
+    private int adminID;
+
+    public Voucher() {
+    }
+
+    public Voucher(int voucherID, String voucherName, int voucherValue, int quantity, int minimumPurchaseAmount, String dateCreated, int duration, int adminID) {
+        this.voucherID = voucherID;
+        this.voucherName = voucherName;
+        this.voucherValue = voucherValue;
+        this.quantity = quantity;
+        this.minimumPurchaseAmount = minimumPurchaseAmount;
+        this.dateCreated = dateCreated;
+        this.duration = duration;
+        this.adminID = adminID;
+    }
+
+    public int getVoucherID() {
+        return voucherID;
+    }
+
+    public void setVoucherID(int voucherID) {
+        this.voucherID = voucherID;
+    }
+
+    public String getVoucherName() {
+        return voucherName;
+    }
+
+    public void setVoucherName(String voucherName) {
+        this.voucherName = voucherName;
+    }
+
+    public int getVoucherValue() {
+        return voucherValue;
+    }
+
+    public void setVoucherValue(int voucherValue) {
+        this.voucherValue = voucherValue;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getMinimumPurchaseAmount() {
+        return minimumPurchaseAmount;
+    }
+
+    public void setMinimumPurchaseAmount(int minimumPurchaseAmount) {
+        this.minimumPurchaseAmount = minimumPurchaseAmount;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+}
