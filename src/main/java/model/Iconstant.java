9f5b08f753b10dff87478e7e2c5bbf5f724f637c
tienminhthanh <thanhtmce181941@fpt.edu.vn>
2025-03-01 15:09:56 +0700

    Merge origin/ThanhMoi2 into ThanhMoi2
    Conflicts:
    	src/main/java/controller/account/ChangePasswordServlet.java
    	src/main/java/controller/account/ProcessPasswordServlet.java
    	src/main/webapp/changePassword.jsp
    	src/main/webapp/processPassword.jsp

--- src/main/java/model/Iconstant.java
+++ src/main/java/model/Iconstant.java
@@ -1,11 +1,13 @@
-
 package model;
 
 public class Iconstant {
-    public static final String GOOGLE_CLIENT_ID = "103840178226-4ev8f05cv55sr4l86jchjtvfd5hvscjb.apps.googleusercontent.com";
-    public static final String GOOGLE_CLIENT_SECRET = "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";
+  public static final String GOOGLE_CLIENT_ID = "103840178226-4ev8f05cv55sr4l86jchjtvfd5hvscjb.apps.googleusercontent.com";
+   public static final String GOOGLE_CLIENT_SECRET = "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";
+
+  
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/loginGoogle";
     public static final String GOOGLE_GRANT_TYPE = "authorization_code";
     public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
 }
