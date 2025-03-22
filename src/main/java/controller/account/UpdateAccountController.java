abf5367d7a2ddc4110b614ae5a1716dbb0bec3ec
tienminhthanh <thanhtmce181941@fpt.edu.vn>
2025-03-22 21:53:05 +0700

    Merge origin/tinh into ThanhMoi2
    Conflicts:
    	src/main/java/controller/event/EventUpdateController.java
    	src/main/java/dao/EventDAO.java
    	src/main/java/dao/OrderDAO.java
    	src/main/java/dao/VoucherDAO.java
    	src/main/java/model/Event.java
    	src/main/java/model/Voucher.java
    	src/main/webapp/WEB-INF/web.xml
    	src/main/webapp/eventDetailsAdmin.jsp

--- src/main/java/controller/account/UpdateAccountController.java
+++ src/main/java/controller/account/UpdateAccountController.java
@@ -87,11 +87,16 @@ String role = "admin".equals(loggedInAccount.getRole()) ? request.getParameter("
 
             }
 
-            if ("addAddress".equals(actionType)) {
+           if ("addAddress".equals(actionType)) {
                 accountDAO.insertNewAddress(loggedInAccount.getAccountID(), selectedAddress);
+                response.sendRedirect("updateAccount?username="+loggedInAccount.getUsername());
+                return;
             } else if ("deleteAddress".equals(actionType)) {
                 int id = Integer.parseInt(selectedAddress);
                 accountDAO.deleteAddress(id);
+                response.sendRedirect("updateAccount?username="+loggedInAccount.getUsername());
+                return;
+               
             }
             // Cập nhật thông tin tài khoản
 
