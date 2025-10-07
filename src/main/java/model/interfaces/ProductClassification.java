2e44f3d97d8cc439234eca87eafdf93d755319d6
anhkc <anhkc@26.138.194.220>
2025-10-07 15:56:49 +0700

    Add common interface for classify product list

---
+++ src/main/java/model/interfaces/ProductClassification.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.interfaces;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ *
+ * @author anhkc
+ */
+public interface ProductClassification {
+    int getId();
+    String getName();
+    String getType();
+    default Map<String,Object> getExtraAttributes(){
+        return Collections.emptyMap();
+    }
+    
+}
