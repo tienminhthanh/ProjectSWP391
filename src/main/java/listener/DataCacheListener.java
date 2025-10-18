4729f8c53e5b3152de607ecaaae7f8adf4783b3b
anhkc <anhkc@26.138.194.220>
2025-10-18 10:27:37 +0700

    Modify access point from ProductDAO to other DAOs to reflect the de-coupling changes

--- src/main/java/listener/DataCacheListener.java
+++ src/main/java/listener/DataCacheListener.java
@@ -1,10 +1,16 @@
- /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/ServletListener.java to edit this template
  */
 package listener;
 
-import dao.ProductDAO;
+import dao.BrandDAO;
+import dao.CategoryDAO;
+import dao.CharacterDAO;
+import dao.CreatorDAO;
+import dao.GenreDAO;
+import dao.PublisherDAO;
+import dao.SeriesDAO;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -19,18 +25,23 @@ public class DataCacheListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext context = sce.getServletContext();
-        ProductDAO productDAO = new ProductDAO();
-        
+        CategoryDAO ctgrDAO = CategoryDAO.getInstance();
+        CreatorDAO creatorDAO = CreatorDAO.getInstance();
+        GenreDAO genreDAO = GenreDAO.getInstance();
+        PublisherDAO publisherDAO = PublisherDAO.getInstance();
+        BrandDAO brandDAO = BrandDAO.getInstance();
+        SeriesDAO seriesDAO = SeriesDAO.getInstance();
+        CharacterDAO characterDAO = CharacterDAO.getInstance();
+
         try {
-            
-        // Load data and store in application scope
-        context.setAttribute("categories", productDAO.getAllCategories());
-        context.setAttribute("creators", productDAO.getAllCreators());
-        context.setAttribute("genres", productDAO.getAllGenres());
-        context.setAttribute("publishers", productDAO.getAllPublishers());
-        context.setAttribute("brands", productDAO.getAllBrands());
-        context.setAttribute("series", productDAO.getAllSeries());
-        context.setAttribute("characters", productDAO.getAllCharacters());
+            // Load data and store in application scope
+            context.setAttribute("categories", ctgrDAO.getAllClassficationEntitiesWithCount());
+            context.setAttribute("creators", creatorDAO.getAllClassficationEntitiesWithCount());
+            context.setAttribute("genres", genreDAO.getAllClassficationEntitiesWithCount());
+            context.setAttribute("publishers", publisherDAO.getAllClassficationEntitiesWithCount());
+            context.setAttribute("brands", brandDAO.getAllClassficationEntitiesWithCount());
+            context.setAttribute("series", seriesDAO.getAllClassficationEntitiesWithCount());
+            context.setAttribute("characters", characterDAO.getAllClassficationEntitiesWithCount());
         } catch (Exception e) {
             System.out.println(e.toString());
         }
