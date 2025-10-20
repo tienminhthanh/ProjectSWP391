0aeb8de9ff2ce9f87061ae7ccafabd7d1855c670
anhkc <anhkc@26.138.194.220>
2025-10-20 17:14:55 +0700

    Add methods for ManageProductList in IGeneralProductDAO

--- src/main/java/dao/interfaces/IGeneralProductDAO.java
+++ src/main/java/dao/interfaces/IGeneralProductDAO.java
@@ -7,8 +7,8 @@ package dao.interfaces;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import model.interfaces.ProductClassification;
 import model.product_related.Product;
+import model.interfaces.IProductClassification;
 
 /**
  *
@@ -18,15 +18,19 @@ public interface IGeneralProductDAO {
 
     Product getProductById(int productID) throws SQLException;
 
-    List<Product> getClassifiedProductList(ProductClassification clsf, String sortCriteria, Map<String, String> filterMap, int page, int pageSize, boolean isHomepage) throws SQLException;
+    List<Product> getClassifiedProductList(IProductClassification clsf, String sortCriteria, Map<String, String> filterMap, int page, int pageSize, boolean isHomepage) throws SQLException;
 
-    int countClassifiedProductList(ProductClassification clsf, Map<String, String> filterMap) throws SQLException;
+    int countClassifiedProductList(IProductClassification clsf, Map<String, String> filterMap) throws SQLException;
 
     List<Product> getRankedProducts(String type) throws SQLException;
 
     List<Product> getSearchResult(String query, String type, String sortCriteria, Map<String, String> filterMap, int page, int pageSize) throws SQLException;
 
     int countSearchResult(String query, String type, Map<String, String> filterMap) throws SQLException;
+
+    List<Product> getAllProducts(String query, String type, String sortCriteria, int page, int pageSize) throws SQLException;
+
+    int countProductListForManagement(String query, String type) throws SQLException;
 
     boolean changeProductStatus(int productID, boolean newStatus) throws SQLException;
 }
